a659afc61927aba92ffa1714467a89bb15adb603
David Vega <david.vega@crowdint.com>
2012-12-07 16:53:45 -0600

    Now it can read files and convert data to float

--- mean_estimator.rb
+++ mean_estimator.rb
@@ -1,3 +1,11 @@
+def read_file file_name
+  lines = []
+  File.open(file_name, "r").each_line do |line|
+    lines << line.to_f
+  end
+  lines
+end
+
 def estimate x, mean, rho
   estimated_value = mean*rho + (1-rho)*x
 end
--- spec/mean_estimator_spec.rb
+++ spec/mean_estimator_spec.rb
@@ -5,3 +5,11 @@ describe 'mean estimator' do
     estimate(3, 2, 0.4).should == 2.5999999999999996
   end
 end
+
+describe 'read files' do
+  it 'should read files'do
+    file = read_file 'read_test.dat'
+    file[2].should == 2.0
+    file[4].should == 4.0
+  end
+end
